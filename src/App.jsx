7f9e6c11dc77baf39f6d31487d79f248c80815de
MuhammadRafliAlfarisqi <farisrafli27@gmail.com>
2024-11-26 14:32:00 +0700

    Fix Resultcard, HeroSection, ButtonHeroSection, About, Services

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import HeroSection from './components/HeroSection';
 import ResultCard from './components/Resultcard';
@@ -24,18 +23,3 @@ const App = () => {
 };
 
 export default App;
-=======
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer";
-
-function App() {
-  return (
-    <section className="hero-section bg-cover bg-center bg-no-repeat h-screen pt-2" style={{ backgroundImage: "url('./assets/images/background-hero-section.png')" }}>
-      <Navbar/>
-      <Footer />
-    </section>
-  );
-}
-
-export default App;
->>>>>>> b22d26776ed6e3742eef72d14841b268142a2e11
